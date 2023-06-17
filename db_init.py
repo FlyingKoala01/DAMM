c00ac8e8106425dbd78dcfccc33da52287a0e2cc
royalmo <royalmo6@gmail.com>
2023-06-17 22:12:35 +0200

    Small fixes on production

--- db_init.py
+++ db_init.py
@@ -2,19 +2,21 @@
 from app import app, db
 import os, utils
 
-# # Remove the database file if it exists
-# if os.path.exists('app/damm.db'):
-#     os.remove('app/damm.db')
+# Remove the database file if it exists
+if os.path.exists('app/damm.db'):
+    print("Estás a punto de eliminar la base de datos. Volver a crearla es una operación MUY LENTA (dura DÍAS).")
+    input("Presiona ENTER para proceder, Ctrl+C para cancelar.")
+    os.remove('app/damm.db')
 
-# # Create app tables
-# with app.app_context():
-#     db.create_all()
+# Create app tables
+with app.app_context():
+    db.create_all()
 
-# # Connect to the database and populate them
-# utils.script_excel_to_sql.main()
-# print("Base de datos lista. Añade direcciones de establecimientos manualmente en otro terminal.")
-# input("Una vez hecho, presione ENTER para cargar las valoraciones de Google.")
-# utils.scrapper.monthly_review_all_months()
+# Connect to the database and populate them
+utils.script_excel_to_sql.main()
+print("Base de datos lista. Añade direcciones de establecimientos manualmente en otro terminal.")
+input("Una vez hecho, presione ENTER para cargar las valoraciones de Google.")
+utils.scrapper.monthly_review_all_months()
 
 utils.rate.calc_rate_all_months()
 utils.rate.cache_last_note()
