4170559ba48ddcf5bde90b420b539f2aec19c119
royalmo <royalmo6@gmail.com>
2023-06-17 20:43:18 +0200

    Finished views

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, url_for, redirect, make_response, send_from_directory, request
-from ..models import Establecimiento as Bar
+from ..models import Establecimiento
 from sqlalchemy import desc, asc
 
 app = Blueprint('main', __name__)
@@ -11,23 +11,10 @@ def index():
 @app.route('/leaderboard', methods=['GET'])
 def leaderboard():
     page = request.args.get('page', 1, type=int)
-    per_page = 8
-    sort_by = request.args.get('sort_by', 'position', type=str)
-    sort_order = request.args.get('sort_order', 'asc', type=str)
-    
-    column_mapping = {
-        'position': Bar.position,
-        'name': Bar.name,
-        'grade': Bar.grade,
-        'sales': Bar.total_sales,
-        'progress': Bar.progress
-    }
+    per_page = 10
+    sort_order = request.args.get('sort_order', 'desc', type=str)
+    sort_function = desc if sort_order=='desc' else asc
+    filter_name = request.args.get('q', '', type=str)
 
-    if sort_order == 'desc':
-        bars_page = Bar.query.order_by(desc(column_mapping[sort_by])).paginate(page, per_page, error_out=False)
-    elif sort_order == 'asc':
-        bars_page = Bar.query.order_by(asc(column_mapping[sort_by])).paginate(page, per_page, error_out=False)
-    else:
-        bars_page = Bar.query.order_by(sort_order(column_mapping[sort_by])).paginate(page, per_page, error_out=False)
-
-    return render_template('pages/leaderboard.html.j2', bars_page=bars_page, sort_order=sort_order, sort_by=sort_by)
+    bars_page = Establecimiento.query.filter(Establecimiento.nombre.contains(filter_name)).order_by(sort_function(Establecimiento.nota)).paginate(page, per_page, error_out=False)
+    return render_template('pages/leaderboard.html.j2', bars_page=bars_page, sort_order=sort_order, filter_name=filter_name)
